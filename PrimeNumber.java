056f42a2bc1fcafc2859eb2a8a1001e89d6d9092
rishika251 <rishikasadineni@gmail.com>
2022-11-20 12:03:51 +0530

    Added:Ability to find prime number between range entered in input

---
+++ PrimeNumber.java
@@ -0,0 +1,35 @@
+package com.bridgelabz.DataStructureproblems;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+	static int lower, upper, number, temp, i, count;
+
+	public void isPrime() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the lower limit");
+		int low = scanner.nextInt();
+		System.out.println("Enter the upper limit");
+		int upp = scanner.nextInt();
+		System.out.println("PrimeNumbers between " + low + " to " + upp);
+		for (number = low; number <= upp; number++) {
+			count = 0;
+			temp = number;
+			for (i = 1; i <= temp; i++) {
+				if (temp % i == 0) {
+					count++;
+				}
+			}
+			if (count == 2) {
+				System.out.print(number + " ");
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		PrimeNumber primeNumber = new PrimeNumber();
+		primeNumber.isPrime();
+
+	}
+}
+
